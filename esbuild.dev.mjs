874aa22313b1bab7337ccb28019e00552675bd52
Pavol Noha <pavol.noha@gmail.com>
2022-10-18 18:36:34 -1000

    Fix talisman build (react)

--- esbuild.dev.mjs
+++ esbuild.dev.mjs
@@ -1,26 +1,29 @@
-import esbuild from "esbuild";
+import esbuild from 'esbuild';
+import { fixTalismanEsm } from './talisman.mjs';
+
+fixTalismanEsm();
 
 /**
  * Esbuild currently only inspects the following fields in tsconfig.json files
  * See https://esbuild.github.io/content-types/#tsconfig-json
  */
 esbuild.build({
-  entryPoints: ["src/index.ts"],
+  entryPoints: ['src/index.ts'],
   bundle: true,
-  format: "esm",
-  platform: "browser",
-  target: "esnext",
+  format: 'esm',
+  platform: 'browser',
+  target: 'esnext',
   preserveSymlinks: true,
   treeShaking: true,
   sourcemap: true,
-  watch: process.env.ESBUILD_WATCH === "true" && {
+  watch: process.env.ESBUILD_WATCH === 'true' && {
     onRebuild(error, result) {
       if (error) {
-        console.error("watch build failed:", error);
+        console.error('watch build failed:', error);
       } else {
-        console.log("watch build succeeded:", result);
+        console.log('watch build succeeded:', result);
       }
     },
   },
-  outdir: "out/",
+  outdir: 'out/',
 });
