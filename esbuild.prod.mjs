32503c13847bab8f623d88b822cb9ac5725d06b2
Pavol Noha <pavol.noha@gmail.com>
2022-10-24 22:26:21 -1000

    Cleanup PROD build, remove gh scripts

--- esbuild.prod.mjs
+++ esbuild.prod.mjs
@@ -1,24 +1,21 @@
 import esbuild from 'esbuild';
+import { fixTalismanEsm } from './talisman.mjs';
 import fs from 'fs';
 import path from 'path';
-import {fileURLToPath} from 'url';
-import {htmlPlugin} from '@craftamap/esbuild-plugin-html';
-import {parse} from 'node-html-parser';
+import { fileURLToPath } from 'url';
+import { htmlPlugin } from '@craftamap/esbuild-plugin-html';
+import { parse } from 'node-html-parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const indexTemplate = fs.readFileSync(
-  path.resolve(__dirname, 'index.html'),
-  'utf8'
-);
+const indexTemplate = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf8');
 
 const indexDOM = parse(indexTemplate);
-const baseNode = indexDOM.getElementsByTagName('base')[0];
-const newBaseNode = baseNode.setAttribute('href', process.env.BASE_HREF || '/');
 const script = indexDOM.getElementsByTagName('script')[0];
-indexDOM.exchangeChild(baseNode, newBaseNode);
 script.remove();
+
+fixTalismanEsm();
 
 esbuild.build({
   entryPoints: ['src/index.ts'],
