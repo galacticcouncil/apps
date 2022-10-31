7e29e1bdd46709b75db36edb6d5e7c2574bce6c6
Pavol Noha <pavol.noha@gmail.com>
2022-10-30 20:32:38 -1000

    Transaction signAndSend cb's

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -1,5 +1,6 @@
 import type { Transaction } from '@galacticcouncil/sdk';
 import type { RuntimeDispatchInfo } from '@polkadot/types/interfaces';
+import type { ISubmittableResult } from '@polkadot/types/types';
 import { getWalletBySource } from '@talismn/connect-wallets';
 import { Account, chainCursor } from '../db';
 
@@ -9,21 +10,18 @@ export async function getPaymentInfo(transaction: Transaction, account: Account)
   return await transactionExtrinsic.paymentInfo(account.address);
 }
 
-export async function signAndSend(transaction: Transaction, account: Account) {
+export async function signAndSend(
+  transaction: Transaction,
+  account: Account,
+  onStatusChange: (status: ISubmittableResult) => void,
+  onError: (error: unknown) => void
+) {
   const api = chainCursor.deref().api;
   const transactionExtrinsic = api.tx(transaction.hex);
   const wallet = getWalletBySource(account.provider);
   await wallet.enable('test1');
 
-  transactionExtrinsic
-    .signAndSend(account.address, { signer: wallet.signer }, ({ status }) => {
-      if (status.isInBlock) {
-        console.log(`Completed at block hash #${status.asInBlock.toString()}`);
-      } else {
-        console.log(`Current status: ${status.type}`);
-      }
-    })
-    .catch((error: any) => {
-      console.log(':( transaction failed', error);
-    });
+  transactionExtrinsic.signAndSend(account.address, { signer: wallet.signer }, onStatusChange).catch((error: any) => {
+    onError(error);
+  });
 }
