3a282ca47286f791622d2988ed575dc463f3cf12
lumir-mrkva <lumir.mrkva@topmonks.com>
2022-12-06 15:13:30 +0100

    rococo rpc

--- src/screen/trade.ts
+++ src/screen/trade.ts
@@ -8,7 +8,7 @@ export class TradeScreen extends LitElement {
   render() {
     return html`
       <gc-trade-app
-        apiAddress="wss://hydradx-devnet-rpc.play.hydration.cloud"
+        apiAddress="wss://hydradx-rococo-rpc.play.hydration.cloud"
         accountAddress="7NPoMQbiA6trJKkjB35uk96MeJD4PGWkLQLH7k7hXEkZpiba"
         accountProvider="polkadot-js"
         accountName="alice"
