03c93d49f79eb715e46288c08b1356b09faa9332
Pavol Noha <pavol.noha@gmail.com>
2022-12-20 14:53:18 +0100

    Trade screen aIn & aIut router support

--- src/screen/trade.ts
+++ src/screen/trade.ts
@@ -1,10 +1,20 @@
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
+import { BeforeEnterObserver, RouterLocation } from '@vaadin/router';
 
 import '../component/trade';
 
 @customElement('gc-trade-screen')
-export class TradeScreen extends LitElement {
+export class TradeScreen extends LitElement implements BeforeEnterObserver {
+  @state() assetIn: string = null;
+  @state() assetOut: string = null;
+
+  async onBeforeEnter(location: RouterLocation) {
+    const queryParams = new URLSearchParams(location.search);
+    this.assetIn = queryParams.get('assetIn');
+    this.assetOut = queryParams.get('assetOut');
+  }
+
   render() {
     return html`
       <gc-trade-app
@@ -14,6 +24,8 @@ export class TradeScreen extends LitElement {
         accountName="alice"
         pools="Omni"
         stableCoinAssetId="2"
+        assetIn=${this.assetIn}
+        assetOut=${this.assetOut}
       ></gc-trade-app>
     `;
   }
