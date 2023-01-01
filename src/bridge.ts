11925f53204b6b3d41be33316defea143a186aea
Pavol Noha <pavol.noha@gmail.com>
2023-01-01 15:18:55 +0100

    Acala EVM adapter support

--- src/bridge.ts
+++ src/bridge.ts
@@ -8,22 +8,9 @@ import { firstValueFrom } from 'rxjs';
 
 import { bridgeCursor } from './db';
 
-const ADAPTERS: Record<string, BaseCrossChainAdapter> = {
-  polkadot: new PolkadotAdapter(),
-  rococo: new RococoAdapter(),
-  acala: new AcalaAdapter(),
-  karura: new KaruraAdapter(),
-  hydradx: new HydradxAdapter(),
-  basilisk: new BasiliskAdapter(),
-};
-
 const CHAINS: Record<string, string[]> = {
   polkadot: ['wss://rpc.polkadot.io'],
-  acala: [
-    'wss://acala-rpc-0.aca-api.network',
-    'wss://acala-rpc-1.aca-api.network',
-    'wss://acala-rpc-2.aca-api.network',
-  ],
+  acala: ['wss://acala-polkadot.api.onfinality.io/public-ws'],
   karura: [
     'wss://karura-rpc-0.aca-api.network',
     'wss://karura-rpc-1.aca-api.network',
@@ -40,6 +27,15 @@ const CHAINS_TESTNET: Record<string, string[]> = {
   basilisk: ['wss://rococo-basilisk-rpc.hydration.dev'],
 };
 
+const ADAPTERS: Record<string, BaseCrossChainAdapter> = {
+  polkadot: new PolkadotAdapter(),
+  rococo: new RococoAdapter(),
+  acala: new AcalaAdapter(CHAINS['acala']),
+  karura: new KaruraAdapter(),
+  hydradx: new HydradxAdapter(),
+  basilisk: new BasiliskAdapter(),
+};
+
 export async function createBridge(chains: string[], testnet: Boolean) {
   const adapters = chains.map((chain: string) => ADAPTERS[chain]);
   const bridge = new Bridge({
@@ -47,7 +43,7 @@ export async function createBridge(chains: string[], testnet: Boolean) {
   });
   const provider = new ApiProvider();
   const chainNames = chains as ChainName[];
-  const connected = provider.connectFromChain(chainNames, testnet ? CHAINS_TESTNET : undefined);
+  const connected = provider.connectFromChain(chainNames, testnet ? CHAINS_TESTNET : CHAINS);
   await firstValueFrom(connected);
   await Promise.all(chains.map((chain) => ADAPTERS[chain].setApi(provider.getApi(chain))));
   bridgeCursor.reset(bridge);
