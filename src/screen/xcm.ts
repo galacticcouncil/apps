8a6731fc72e8ef3fe093994f0583ff6eb82fc8ed
Pavol Noha <pavol.noha@gmail.com>
2023-01-05 19:54:09 +0100

    Rollback acala removal

--- src/screen/xcm.ts
+++ src/screen/xcm.ts
@@ -10,7 +10,7 @@ export class XcmScreen extends LitElement {
       <gc-xcm-app
         srcChain="polkadot"
         dstChain="hydradx"
-        chains="polkadot,hydradx"
+        chains="polkadot,hydradx,acala"
         accountAddress="7NPoMQbiA6trJKkjB35uk96MeJD4PGWkLQLH7k7hXEkZpiba"
         accountProvider="polkadot-js"
         accountName="alice"
