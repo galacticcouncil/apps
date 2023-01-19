5d4b6070296d64ca43045113c2b800671c7b782d
Pavol Noha <pavol.noha@gmail.com>
2023-01-19 15:29:07 +0100

    Skip formating if amount integer

--- src/utils/amount.ts
+++ src/utils/amount.ts
@@ -7,6 +7,11 @@ export function formatAmount(amount: BigNumber, decimals: number): string {
 
 export function humanizeAmount(amount: string): string {
   const amountNo = Number(amount);
+  if (Number.isInteger(amountNo)) {
+    const formattedNo = new Intl.NumberFormat('en-US').format(amountNo);
+    return formattedNo.replaceAll(',', ' ');
+  }
+
   let maxSignDigits: number = 4;
   if (amountNo > 1) {
     const intPartLen = Math.ceil(Math.log10(Number(amountNo) + 1));
