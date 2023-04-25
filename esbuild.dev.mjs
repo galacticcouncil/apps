5df42d92098d8df7f05fc6cd6cb4ba69c1a3329d
Pavol Noha <pavol.noha@gmail.com>
2023-04-25 14:42:39 +0200

    Dev server ns (refactor)

--- esbuild.dev.mjs
+++ esbuild.dev.mjs
@@ -1,6 +1,6 @@
 import esbuild from 'esbuild';
-import http from 'node:http';
 import { fixTalismanEsm } from './talisman.mjs';
+import { createProxyServer } from './server.dev.mjs';
 
 fixTalismanEsm();
 
@@ -23,36 +23,4 @@ const ctx = await esbuild.context({ ...options, plugins });
 await ctx.rebuild();
 await ctx.watch();
 const localServer = await ctx.serve({ servedir: './',  host: '127.0.0.1' });
-
-const PROXY_PORT = 3000;
-
-/**
- * Create proxy server that will forward requests to esbuild local server.
- * @see https://esbuild.github.io/api/#serve-proxy
- */
-http.createServer((req, res) => {
-  const forwardRequest = (path) => {
-    const options = {
-      hostname: localServer.host,
-      port: localServer.port,
-      path,
-      method: req.method,
-      headers: req.headers,
-    };
-
-    const proxyReq = http.request(options, (proxyRes) => {
-      // If esbuild local server return 404, use SPA router config for fallback
-      if (proxyRes.statusCode === 404) {
-        return forwardRequest("/");
-      }
-
-      res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    });
-
-    req.pipe(proxyReq, { end: true });
-  };
-  forwardRequest(req.url);
-}).listen(PROXY_PORT)
-
-console.log('\x1b[1m\x1b[92m', '> Proxy: \x1b[4mhttp://' + localServer.host + ':' + PROXY_PORT + '/\x1b[0m\n');
+createProxyServer(localServer);
