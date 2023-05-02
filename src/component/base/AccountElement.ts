707d6d7c8a4a09f2f5f8debb889f7e1ccb3f13e7
Pavol Noha <pavol.noha@gmail.com>
2023-05-02 15:26:34 +0200

    AccountElement impl

---
+++ src/component/base/AccountElement.ts
@@ -0,0 +1,44 @@
+import { LitElement } from 'lit';
+import { property } from 'lit/decorators.js';
+
+import { Account, accountCursor } from '../../db';
+import { DatabaseController } from '../../db.ctrl';
+
+export abstract class AccountElement extends LitElement {
+  protected account = new DatabaseController<Account>(this, accountCursor);
+
+  @property({ type: String }) accountAddress: string = null;
+  @property({ type: String }) accountProvider: string = null;
+  @property({ type: String }) accountName: string = null;
+
+  protected abstract onAccountChange(prev: Account, curr: Account): Promise<void>;
+
+  private updateAccount() {
+    if (this.accountAddress && this.accountProvider) {
+      accountCursor.reset({
+        address: this.accountAddress,
+        provider: this.accountProvider,
+        name: this.accountName,
+      } as Account);
+    } else {
+      accountCursor.reset(null);
+    }
+  }
+
+  override update(changedProperties: Map<string, unknown>) {
+    if (changedProperties.has('accountAddress') || changedProperties.has('accountProvider')) {
+      this.updateAccount();
+    }
+    super.update(changedProperties);
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    accountCursor.addWatch('account-watch', (_id, prev, curr) => this.onAccountChange(prev, curr));
+  }
+
+  override disconnectedCallback() {
+    accountCursor.removeWatch('account-watch');
+    super.disconnectedCallback();
+  }
+}
