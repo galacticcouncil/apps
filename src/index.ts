957760a3a794af5b06bd17b8b68f76e294958ccd
Pavol Noha <pavol.noha@gmail.com>
2023-06-08 13:09:45 +0200

    Export DCA App

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { TradeApp } from './component/trade';
+export { DcaApp } from './component/dca';
 export { XcmApp } from './component/xcm';
 export { NotificationCenter } from './component/notification';
 export { TransactionCenter } from './component/transaction';
