d7b65bcf3978cff78d7520887affded679a25db6
Pavol Noha <pavol.noha@gmail.com>
2023-06-08 13:07:33 +0200

    Upgrade sdk to 0.2.2

--- src/api/asset.ts
+++ src/api/asset.ts
@@ -1,11 +1,11 @@
-import { Amount, AssetApiClient, AssetDetail, PoolAsset, BalanceApiClient, AssetMetadata } from '@galacticcouncil/sdk';
+import { Amount, AssetClient, AssetDetail, PoolAsset, BalanceClient, AssetMetadata } from '@galacticcouncil/sdk';
 
 import { chainCursor } from '../db';
 import { pairs2Map } from '../utils/mapper';
 
 export async function getAssetsMeta(assets: PoolAsset[]) {
   const api = chainCursor.deref().api;
-  const assetClient = new AssetApiClient(api);
+  const assetClient = new AssetClient(api);
   const details: [string, AssetMetadata][] = await Promise.all(
     assets.map(async (asset: PoolAsset) => [asset.id, await assetClient.getAssetMetadata(asset.id)])
   );
@@ -14,7 +14,7 @@ export async function getAssetsMeta(assets: PoolAsset[]) {
 
 export async function getAssetsDetail(assets: PoolAsset[]) {
   const api = chainCursor.deref().api;
-  const assetClient = new AssetApiClient(api);
+  const assetClient = new AssetClient(api);
   const details: [string, AssetDetail][] = await Promise.all(
     assets.map(async (asset: PoolAsset) => [asset.id, await assetClient.getAssetDetail(asset.id)])
   );
@@ -23,7 +23,7 @@ export async function getAssetsDetail(assets: PoolAsset[]) {
 
 export async function getAssetsBalance(address: string, assets: PoolAsset[]) {
   const api = chainCursor.deref().api;
-  const balanceClient = new BalanceApiClient(api);
+  const balanceClient = new BalanceClient(api);
   const balances: [string, Amount][] = await Promise.all(
     assets.map(async (asset: PoolAsset) => [asset.id, await balanceClient.getAccountBalance(address, asset.id)])
   );
