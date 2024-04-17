d1772a71eccaa2f9fdc274e83bebcc895cee9e06
Jakub Vonášek <vonasek.jakub@gmail.com>
2024-04-17 15:38:33 +0200

    Fixed chart container height jump, fixed asset switch button hover in Safari

--- packages/apps/src/element/chart/Chart.ts
+++ packages/apps/src/element/chart/Chart.ts
@@ -216,6 +216,7 @@ export abstract class Chart extends BaseElement {
 
   override async firstUpdated() {
     this.chartContainer = this.shadowRoot.getElementById('chart');
+    this.chartContainer.style.height = `${CHART_HEIGHT}px`;
     this.chart = createChart(this.chartContainer, {
       layout: layoutOptions,
       rightPriceScale: rightPriceScale,
